99b79f07123bd0c89651bef3ae779a6ceb605423
Antonio Hernandez <antoniohernandezleon@MacBook-Pro.local>
2023-12-29 12:38:46 +0100

    Creamos archivo 04-funciones.ts y sus compilados

---
+++ poo-y-typescript/js/04-funciones.js
@@ -0,0 +1,11 @@
+// Funciones en TS
+/**
+ * function nombreMetodo (parametro: tipoVariable) :tipoVariableReturn
+ *
+ * @param numero
+ * @returns
+ */
+function getNumero(numero) {
+    return "El numero es " + numero;
+}
+console.log(getNumero(55));
---
+++ poo-y-typescript/js/04-funciones.ts
@@ -0,0 +1,13 @@
+// Funciones en TS
+/**
+ * function nombreMetodo (parametro: tipoVariable) :tipoVariableReturn
+ * 
+ * @param numero 
+ * @returns 
+ */
+
+function getNumero(numero: number):string{
+    return "El numero es "+numero;
+}
+
+console.log(getNumero(55));
